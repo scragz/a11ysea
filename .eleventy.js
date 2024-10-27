f325f6149eeba9f8c3569482656af3120954e9ef
Jaycee Lydian <scragz@users.noreply.github.com>
2024-10-27 15:09:09 -0700

    remove image bs entirely

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ const sass = require("sass");
 const path = require("path");
 const browserslist = require("browserslist");
 const { transform, browserslistToTargets } = require("lightningcss");
-const Image = require("@11ty/eleventy-img");
 
 module.exports = function(eleventyConfig) {
   // Copy the CSS and JS folders to the output
@@ -80,25 +79,6 @@ module.exports = function(eleventyConfig) {
         return code;
       };
     },
-  });
-
-  // Add image shortcode
-  eleventyConfig.addShortcode("image", async function (src, alt, widths = [300, 800], sizes = "100vh") {
-    let metadata = await Image(src, {
-      widths,
-      formats: ["webp", "jpeg"],
-      urlPath: "/assets/images/",
-      outputDir: "_site/assets/images/",
-    });
-
-    let imageAttributes = {
-      alt,
-      sizes,
-      loading: "lazy",
-      decoding: "async",
-    };
-
-    return Image.generateHTML(metadata, imageAttributes);
   });
 
   return {
